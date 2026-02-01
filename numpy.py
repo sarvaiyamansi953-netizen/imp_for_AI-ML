273b71fc7cf4a848b37bf5cac1e2574a58236b41
Mansi Sarvaiya <sarvaiyamansi953@gmail.com>
2026-02-01 14:57:19 +0530

    Add files via upload

---
+++ numpy.py
@@ -0,0 +1,14 @@
+#Array Creation 
+import numpy as np
+arr = np.array([1, 2, 3, 4])
+print(arr)
+
+#matrix operations on 2D arrays
+import numpy as np
+
+a = np.array([[1, 2], [3, 4]])
+b = np.array([[5, 6], [7, 8]])
+
+print("Addition:\n", a + b)
+print("Multiplication:\n", a * b)
+print("Dot Product:\n", np.dot(a, b))
